aa3dc2bd92cf8fe9c0a1b21bc4f6b432a10365e7
officialjewel57 <loganpersonal29@gmail.com>
2026-03-01 19:25:57 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 Game.registerMod("harder_core",{
     init:function(){
       Game.Notify("Unluckiest mod ever has loaded!", "why did i add the exclamation mark because this isnt an exciting mod :sob:", [16, 5]);
+      let a = Game.shimmerTypes.golden.popFunc.toString();
+      let b = a.replace("var choice=choose(list)", "var choice='blab';");
+      eval('Game.shimmerTypes.golden.popFunc=' + b);
+      Math.random = () => 0.9999999999999999;
       Game.registerHook('logic', function() {
-        Game.lumpCurrentType = 0;
-        Math.random = () => 0.9999999999999999;
-        let a = Game.shimmerTypes.golden.popFunc.toString();
-        let b = a.replace("var choice=choose(list)", "var choice='blab';");
-        eval('Game.shimmerTypes.golden.popFunc=' + b);
+        if(Game.lumpCurrentType > 0){
+          Game.lumpCurrentType = 0;
+        }
       });
     },
     save:function(){
